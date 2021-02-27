84fb2df1e6eb1ea43eea46dbd3daa6ebe3da07ab
kody.oneill <koneill6@my.gcu.edu>
2021-02-27 17:05:28 -0600

    Database_added_Login_added

--- src/controllers/RegistrationFormController.java
+++ src/controllers/RegistrationFormController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.sql.SQLException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
@@ -7,11 +9,15 @@ import beans.User;
 
 @ManagedBean
 public class RegistrationFormController {
-	public String onSubmit() {
+	public String onSubmit() throws SQLException {
 
 		FacesContext context = FacesContext.getCurrentInstance();
 		User user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
 
+		//adding user to DB
+		DatabaseService ds = new DatabaseService();
+		ds.insertOne(user);
+		
 		// use for testing
 		System.out.println(user.getFirstName());
 
