7c886d3d69e6e7ca2f0b0160652ae9d4fbcfffdc
Brydon Johnson <brykeith@gmail.com>
2021-03-12 23:48:10 -0700

    finished animation on menu icon

--- WebContent/resources/js/_header.js
+++ WebContent/resources/js/_header.js
@@ -8,4 +8,6 @@ const mobileNav = document.querySelector(".nav--mobile");
 // adds and removes the "change" class from the navbar when you click on the hamburger-menu
 	menuIcon.addEventListener("click", () => {
   	mobileNav.classList.toggle("active");
+	menuIcon.classList.toggle("open");
+	
 });
